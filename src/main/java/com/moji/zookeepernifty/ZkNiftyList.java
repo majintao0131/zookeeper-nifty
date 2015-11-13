a072265e84639509e1a4f6b38988f9f50e97be7a
heyaxiang <six.heyaxiang@gmail.com>
2015-11-13 22:02:34 +0800

    add lock

--- src/main/java/com/moji/zookeepernifty/ZkNiftyList.java
+++ src/main/java/com/moji/zookeepernifty/ZkNiftyList.java
@@ -2,12 +2,17 @@ package com.moji.zookeepernifty;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ZkNiftyList<T> {
 	private Queue<T> queue = new ConcurrentLinkedQueue<T>();
+	private Lock lock = null;
 	
 	public ZkNiftyList() {
-		queue.clear();
+		if (lock == null) {
+			lock = new ReentrantLock();
+		}
 	}
 	
 	public void addClient(T client) {
@@ -28,5 +33,17 @@ public class ZkNiftyList<T> {
 	
 	public int getCount() {
 		return queue.size();
+	}
+	
+	public Queue<T> getQuene() {
+		return queue;
+	}
+	
+	public void Lock() {
+		this.lock.lock();
+	}
+	
+	public void UnLock() {
+		this.lock.unlock();
 	}
 }
